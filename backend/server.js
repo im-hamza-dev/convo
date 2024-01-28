dfa90a53b58f3fbbda80b04dc86b8909e4646aa4
im-hamza-dev <hi.hamza.dev@gmail.com>
2024-01-28 19:46:26 +0500

    removed env

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,6 @@ const app = express();
 
 app.use(express.json()); // to accept json data
 app.use(cors());
-// app.get("/", (req, res) => {
-//   res.send("API Running!");
-// });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
